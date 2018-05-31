597c388efedf33c9e45316fe2c825ffcdc9b3015
EngleLin <engle0232@gmail.com>
2018-05-31 16:01:45 +0800

    新增刪除

--- 0503/del.php
+++ 0503/del.php
@@ -1,11 +1,6 @@
 <?php
-//error_reporting(0);
-//修改 php \ php.ini 中 702 行
-// always_populate_raw_post_data = -1
-// 可以關掉 $HTTP_RAW_POST_DATA 停用訊息
-// 記得要關掉重開php或網頁伺服器
 //輸入=======================================================
-//預期收到的資料格式  { prod:"商品的名稱", price:"值 }
+//預期收到的資料格式  { id:值 }
 $data = json_decode(file_get_contents('php://input'));
 //檢查你的資料. 暫時省略
 //資料庫操作===================================================
@@ -21,10 +16,9 @@ try {
 	//echo $err->getMessage(); //測試的時候用
 	exit;
 }
-$stmt = $db->prepare('insert into moneybook (name,cost) values (?,?)');
-$stmt->execute( [ $data->{"prod"}, $data->{"price"} ] );
+$stmt = $db->prepare('delete from moneybook where m_id=?');
+$stmt->execute( [ $data->{"id"} ] );
 //輸出=======================================================
-$data->{"id"} = $db->lastInsertId();  //取得前一次 insert 的 id
-http_response_code(201);
+http_response_code(200);
 header("Content-Type: application/json;charset=UTF-8");
 echo json_encode($data);              //把insert的資料再回傳給用戶端
